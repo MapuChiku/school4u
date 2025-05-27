db608ce67b8e722539b6a9a11f438a28898843e7
MapuChiku <brahmeradha@gmail.com>
2025-05-27 11:07:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
-from scikit-learn import LinearRegression
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-st.title(school4u)
+st.title("Linear Regression with Streamlit")
+
+data = pd.read_csv("data.csv")
+st.write(data)
+
+x = data[['Hours_Studied']]
+y = data['Marks_Scored']
+
+model = LinearRegression()
+model.fit(x, y)
+
+hours = st.number_input("Enter Hours Studied:", 1.0, 10.0, step=0.5)
+prediction = model.predict([[hours]])
+st.write("Predicted Marks:", prediction[0])
